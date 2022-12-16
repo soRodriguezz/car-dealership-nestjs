e80e73378b0d7cd2d5bab5c0cf0c9e1a48cc4f5c
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-12-15 22:46:25 -0300

    crear seed con nest para cargar datos

---
+++ src/seed/seed.module.ts
@@ -0,0 +1,15 @@
+import { Module } from '@nestjs/common';
+import { BrandsModule } from 'src/brands/brands.module';
+import { CarsModule } from 'src/cars/cars.module';
+import { SeedController } from './seed.controller';
+import { SeedService } from './seed.service';
+
+@Module({
+  controllers: [SeedController],
+  providers: [SeedService],
+  imports: [
+    CarsModule,
+    BrandsModule
+  ]
+})
+export class SeedModule {}
